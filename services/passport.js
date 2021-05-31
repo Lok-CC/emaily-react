c56da5f7d9cf7309c2b7351ac65f372b3f3ba78f
LOK CHEE CONG <79251149+Lok-PG@users.noreply.github.com>
2021-05-31 22:18:05 +0800

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -10,10 +10,9 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then((user) => {
-      done(null, user)
-    })
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
 });
 
 passport.use(
@@ -22,6 +21,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
+      proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then((existingUser) => {
